8cba961eb69d81aa38ab29948c5092ad246ee9b2
Adam <adam@Adams-MacBook-Pro.local>
2019-05-28 09:04:39 -0700

    Updates

---
+++ precache-manifest.24fee3ba59af4cfd1fd294adf3fdbcc3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5f8f3aa100eb4476af35622f614e379",
+    "url": "/movie-list/index.html"
+  },
+  {
+    "revision": "ed589a63b2fb68ce56c7",
+    "url": "/movie-list/static/css/2.cc946ccf.chunk.css"
+  },
+  {
+    "revision": "ba5efb589964fcd8fafc",
+    "url": "/movie-list/static/css/main.5c5158b8.chunk.css"
+  },
+  {
+    "revision": "ed589a63b2fb68ce56c7",
+    "url": "/movie-list/static/js/2.ddbafa9f.chunk.js"
+  },
+  {
+    "revision": "ba5efb589964fcd8fafc",
+    "url": "/movie-list/static/js/main.6d5e0f97.chunk.js"
+  },
+  {
+    "revision": "f36c80dcc518bdc2b711",
+    "url": "/movie-list/static/js/runtime~main.de49e91d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/movie-list/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/movie-list/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/movie-list/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/movie-list/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/movie-list/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
